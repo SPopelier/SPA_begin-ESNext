30c1138cb993e270592874d6a8581d5f945bae78
Sarah Popelier <sarah.popelier@email.com>
2025-12-04 09:22:05 +0100

    Import / Export

--- 5-import/script.js
+++ 5-import/script.js
@@ -1,8 +1,17 @@
 //5-1 Import
 
-import {hello} from './functions.js';
+import defaultFunction, { hello, showError } from './functions.js';
+import {sum, sub, multiply, divide, pow } from './math.js';
 
 hello();
+showError();
+defaultFunction();
+sum();
+sub();
+multiply();
+divide();
+pow();
 
 // 5-2 Export
 const [a, b] = [7, 9];
+console.log(sum(a, b));
